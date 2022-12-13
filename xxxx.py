2a987f1bff2eb880f1e454558fc25fa83328c037
Samjidsheikh-1122 <120444741+Samjidsheikh-1122@users.noreply.github.com>
2022-12-13 16:49:05 +0600

    Add files via upload

---
+++ xxxx.py
@@ -0,0 +1,28 @@
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+        os.system('xdg-open https://youtube.com/c/KhaliDTechBd1?utm_source=EKLEiJECCKjOmKnC5IiRIQ')
+ 
+        
+ 
+        
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        os.system('xdg-open https://youtube.com/c/KhaliDTechBd1?utm_source=EKLEiJECCKjOmKnC5IiRIQ')
+ 
+        os.system('python text.py')
